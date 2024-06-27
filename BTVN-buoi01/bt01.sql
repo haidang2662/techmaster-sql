856ff5d7062d95d968a7f7483f70f32934902ea2
haidang2662 <141023574+haidang2662@users.noreply.github.com>
2024-06-27 15:13:32 +0700

    lam bai tap buoi 01

---
+++ BTVN-buoi01/bt01.sql
@@ -0,0 +1,15 @@
+create TABLE Person (
+id int,
+fullName varchar(25),
+job varchar(25),
+birthday DATE,
+salary int,
+address varchar(25)
+);
+
+INSERT INTO person(id,fullname,job,birthday,salary,address)
+values (1,"thanh","sinh_viên","1990-01-01",100,"hn"),
+(2,"hằng","học_sinh","2005-01-01",10,"hp"),
+(3,"hương","giáo_viên","1988-01-01",200,"hcm"),
+(4,"tuấn","bác_sĩ","1986-01-01",300,"hp"),
+(5,"Ngọc","ngân_hàng","1990-01-01",500,"hp");
--- BTVN-buoi01/btvn01.sql
+++
@@ -1,6 +0,0 @@
-create table player (
-id int ,
-name varchar(25),
-dob date,
-job varchar(10)
-);player
